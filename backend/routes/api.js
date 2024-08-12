eed46d6bdba804d2f81559f05663b1e1f01baf6a
rchitk23 <rachitjee9@gmail.com>
2024-08-12 13:33:36 +0530

    Initial commit

--- backend/routes/api.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getContent } = require('../controllers/contentController');
-
-router.get('/content', getContent);
-
-module.exports = router;
